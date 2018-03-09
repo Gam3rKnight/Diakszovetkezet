01812697e3d4de74402a1ddee3492d073e55267a
Gam3rKnight <anthonyfan1022@gmail.com>
2018-03-09 09:24:53 +0100

    v1.1
    bejelntkezési felület, entity framework

--- Diakszovetkezet/structUserData.cs
+++ Diakszovetkezet/structUserData.cs
@@ -12,12 +12,12 @@ namespace Diakszovetkezet
         private static string Email;
         private static string FirstName;
         private static string LastName;
-        private static string Role;
+        private static int Role;
 
         public string userName { get { return UserName; } set { UserName = value; } }
         public string email { get { return Email; } set { Email = value; } }
         public string firstName { get { return FirstName; } set { FirstName = value; } }
         public string lastName { get { return LastName; } set { LastName = value; } }
-        public string role { get { return Role; } set { Role = value; } }
+        public int role { get { return Role; } set { Role = value; } }
     }
 }
--- Diakszovetkezet/wndBejelentkezes.xaml.cs
+++ Diakszovetkezet/wndBejelentkezes.xaml.cs
@@ -15,21 +15,59 @@ using System.Windows.Shapes;
 
 namespace Diakszovetkezet
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    
     public partial class MainWindow : Window
     {
-        DBConnection connection;
+        private structUserData user;
+        wndDiakAblak wndDiak;
+        wndRendszerAdmin wndAdmin;
+      
         public MainWindow()
         {
             InitializeComponent();
-            connection = new DBConnection();
+            user = new structUserData();
+            wndDiak = new wndDiakAblak();
+            wndAdmin = new wndRendszerAdmin();
+        }
+
+        public structUserData User
+        {
+            get { return user; }
+            set { user = value; }
         }
 
         private void btBejelentkezes_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Bejelentkezési kísérlet!", "Info", MessageBoxButton.OK, MessageBoxImage.Information);
+            using (DiakszovetkezetEntities context = new DiakszovetkezetEntities())
+            {
+                var result = from u in context.Users
+                             select u;
+                bool vane = false;
+                
+
+                foreach (var u in result)
+                {
+                    if (tbFelhasznalonev.Text == u.username && pbJelszo.Password == u.password);
+                    {
+                        vane = true;
+                        user.userName = u.username;
+                        user.email = u.email;
+                        user.firstName = u.fname;
+                        user.lastName = u.lname;
+                        user.role = u.role;
+                    }
+                }
+                if (vane&&user.role==1)
+                    {
+                        wndDiak.ShowDialog();
+                    }
+                else if(user.role==0)
+                {
+                    wndAdmin.ShowDialog();
+                }
+                    else MessageBox.Show("Sikertelen bejelentkezés!", "Hiba", MessageBoxButton.OK, MessageBoxImage.Error);
+                
+            }
         }
     }
 }
