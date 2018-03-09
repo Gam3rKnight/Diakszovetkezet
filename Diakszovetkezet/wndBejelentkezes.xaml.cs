2a37fd2fc2cf33df581d14d94afab8786dc7b824
Gam3rKnight <anthonyfan1022@gmail.com>
2018-03-09 12:15:37 +0100

    v1.2
    Működik a bejelentkezés, illetve az ablakok bezárásának eseménykezelőit is megcsináltam.

--- Diakszovetkezet/wndBejelentkezes.xaml.cs
+++ Diakszovetkezet/wndBejelentkezes.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,14 +17,15 @@ using System.Windows.Shapes;
 namespace Diakszovetkezet
 {
     
-    public partial class MainWindow : Window
+    public partial class wndBejelentkezes : Window
     {
         private structUserData user;
         wndDiakAblak wndDiak;
         wndRendszerAdmin wndAdmin;
       
-        public MainWindow()
+        public wndBejelentkezes()
         {
+            
             InitializeComponent();
             user = new structUserData();
             wndDiak = new wndDiakAblak();
@@ -38,36 +40,79 @@ namespace Diakszovetkezet
 
         private void btBejelentkezes_Click(object sender, RoutedEventArgs e)
         {
-            using (DiakszovetkezetEntities context = new DiakszovetkezetEntities())
+            if (tbFelhasznalonev.Text != "" && pbJelszo.Password != "")
             {
-                var result = from u in context.Users
-                             select u;
-                bool vane = false;
-                
+                using (DiakszovetkezetEntities context = new DiakszovetkezetEntities())
+                {
+                    var result = from u in context.Users
+                                 select u;
+                    bool vane = false;
 
-                foreach (var u in result)
-                {
-                    if (tbFelhasznalonev.Text == u.username && pbJelszo.Password == u.password);
+                    foreach (var u in result)
                     {
-                        vane = true;
-                        user.userName = u.username;
-                        user.email = u.email;
-                        user.firstName = u.fname;
-                        user.lastName = u.lname;
-                        user.role = u.role;
+                        if (tbFelhasznalonev.Text == u.username && pbJelszo.Password == u.password)
+                        {
+                            vane = true;
+                            user.userName = u.username;
+                            user.email = u.email;
+                            user.firstName = u.fname;
+                            user.lastName = u.lname;
+                            user.role = u.role;
+                        }
                     }
-                }
-                if (vane&&user.role==1)
+                    if (vane && user.role == 1)
                     {
+                        wndDiak.Closing += Window_Closing;
                         wndDiak.ShowDialog();
+                        tbFelhasznalonev.Clear();
+                        pbJelszo.Clear();
                     }
-                else if(user.role==0)
-                {
-                    wndAdmin.ShowDialog();
-                }
+                    else if (vane && user.role == 0)
+                    {
+                        wndAdmin.Closing += Window_Closing;
+                        wndAdmin.ShowDialog();
+                        tbFelhasznalonev.Clear();
+                        pbJelszo.Clear();
+                    }
                     else MessageBox.Show("Sikertelen bejelentkezés!", "Hiba", MessageBoxButton.OK, MessageBoxImage.Error);
-                
+
+                }
             }
+            else
+            {
+                MessageBox.Show("Nem megfelelően töltötte ki a megjelölt mezőket!", "Hiba", MessageBoxButton.OK, MessageBoxImage.Error);
+                if(tbFelhasznalonev.Text=="")
+                {
+                    tbFelhasznalonev.BorderBrush = Brushes.Red;
+                    tbFelhasznalonev.BorderThickness = new Thickness(2.0);
+                }
+
+                if (pbJelszo.Password == "")
+                {
+                    pbJelszo.BorderBrush = Brushes.Red;
+                    pbJelszo.BorderThickness = new Thickness(2.0);
+                }
+            }
+        }
+        protected override void OnClosing(CancelEventArgs e)
+        {
+           Application.Current.Shutdown();
+        }
+
+        private void pbJelszo_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            pbJelszo.BorderBrush = Brushes.Transparent;
+            pbJelszo.BorderThickness = new Thickness(0, 0, 0, 1);
+        }
+
+        private void tbFelhasznalonev_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            tbFelhasznalonev.BorderBrush = Brushes.Transparent;
+            tbFelhasznalonev.BorderThickness = new Thickness(0, 0, 0, 1);
+        }
+        private void Window_Closing(object sender, CancelEventArgs e)
+        {
+            e.Cancel = (MessageBox.Show("Biztosan be akarja zárni az ablakot?", "Figyelmeztetés", MessageBoxButton.YesNo, MessageBoxImage.Warning, MessageBoxResult.Yes) == MessageBoxResult.No);
         }
     }
 }
